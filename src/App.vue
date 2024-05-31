2fc14e46a51fd7ba9ef3e9149d3dc3c6201bb706
Clement Glt <guillemot.clement@protonmail.com>
2024-05-31 10:41:51 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="firstDiv">
+    <h1>Mon projet</h1>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.firstDiv {
+  min-height: 100vh;
+  background-color: aqua;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
